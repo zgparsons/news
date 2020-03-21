a7130f4443e4c2e5de0e3db6b86a0b864c08a9ff
zgparsons <z.g.parsons@outlook.com>
2020-03-21 10:48:55 +0000

    Add cacheFirst and networkFirst async functions

--- sw.js
+++ sw.js
@@ -4,10 +4,38 @@ const staticAssets = [
     './app.js'
 ];
 
-self.addEventListener('install', event => {
-    console.log('install');
+self.addEventListener('install', async event => {
+    // console.log('install');
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
 })
 
 self.addEventListener('fetch', event => {
-    console.log('fetch');
+    // console.log('fetch');
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if (url.orgin === location.origin) {
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+
 })
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('news-dynamic');
+
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
+}
